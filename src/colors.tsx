444c892950d623b541c1dcd09c8f5671ac212c98
Gadi Jacobson <30384058+gadij@users.noreply.github.com>
2024-12-04 17:12:13 +0200

    adding style

--- src/colors.tsx
+++ src/colors.tsx
@@ -6,6 +6,11 @@ const colors = {
     warning: '#ffc107',
     info: '#17a2b8',
     light: '#f8f9fa',
-    dark: '#343a40'
+    dark: '#343a40',
+    table: {
+        border: '#ddd',
+        body: '#f1f1f1',
+        head: '#f5f5f5',
+    }
 }
 export default colors
